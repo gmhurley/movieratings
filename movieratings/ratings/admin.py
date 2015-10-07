1a3adf3746d8248989d38692abd9b452907cfcc7
Glenn Hurley <glenn.hurley@gmail.com>
2015-10-07 12:37:04 -0400

    Updated rater and ratings modules.

--- movieratings/ratings/admin.py
+++ movieratings/ratings/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Rater, Movies, Ratings
+from .models import Rater, Movies, Ratings, Occupation
 
 # Register your models here.
 admin.site.register(Rater)
 admin.site.register(Movies)
 admin.site.register(Ratings)
+admin.site.register(Occupation)
--- movieratings/ratings/models.py
+++ movieratings/ratings/models.py
@@ -1,21 +1,33 @@
 from django.db import models
 
 # Create your models here.
-# Create Django models for users (call the model Rater so as not to
-# confuse it with Django users), movies, and ratings. Make sure that
-# your models can contain the data from your dataset.
+
+
+class Occupation(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+            return self.title
 
 
 class Rater(models.Model):
-    # userId,movieId,rating,timestamp
-    rater_id = models.CharField(max_length=11)
+    age = models.IntegerField(null=True)
+    # https://en.wikipedia.org/wiki/ISO/IEC_5218
+    # 0 = Unknown, 1 = Male, 2 = Female, 9 = not specified
+    gender = models.CharField(max_length=1)
+    occupation = models.ForeignKey(Occupation)
+    zip_code = models.CharField(max_length=10)
+
+    def __str__(self):
+        return 'Age: {}  Occupation: {}  Zip: {}'.format(self.age,
+                                                         self.occupation,
+                                                         self.zip)
 
 
 class Movies(models.Model):
     class Meta:
         verbose_name_plural = 'movies'
 
-    movie_id = models.CharField(max_length=11)
     title = models.CharField(max_length=255)
     action = models.BooleanField()
     adventure = models.BooleanField()
@@ -36,11 +48,19 @@ class Movies(models.Model):
     war = models.BooleanField()
     western = models.BooleanField()
 
+    def __str__(self):
+        return self.title
+
 
 class Ratings(models.Model):
     class Meta:
         verbose_name_plural = 'ratings'
 
     rater = models.ForeignKey(Rater)
-    movid_id = models.ForeignKey(Movies)
+    movie = models.ForeignKey(Movies)
     rating = models.FloatField()
+
+    def __str__(self):
+        return 'Movie: {}  Rater: {}  Rating: {}'.format(self.movie,
+                                                         self.rater,
+                                                         self.rating)
