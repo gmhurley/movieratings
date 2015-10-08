3b0daea623b5929139328d8ac50513dfe1dbcab9
Glenn Hurley <glenn.hurley@gmail.com>
2015-10-08 14:28:06 -0400

    Fixed average rating and number of ratings so the calcualtions are done in the model.

--- movieratings/ratings/views.py
+++ movieratings/ratings/views.py
@@ -7,7 +7,6 @@ from .models import Movies, Ratings, Rater
 
 def all_movies(request):
     movies = Movies.objects.all()[:20]
-    # titles = [str(title) for title in movies]
     return render(request,
                   'ratings/movies.html',
                   {'movies': movies})
@@ -15,19 +14,7 @@ def all_movies(request):
 
 def movie_detail(request, movie_id):
     movie = Movies.objects.get(pk=movie_id)
-    ratings = movie.ratings_set.all()
-
-    ratings_total = 0.0
-    user_ratings = []
-
-    for rating in ratings:
-        ratings_total += rating.rating
-        user_ratings.append([rating.rater_id, rating.rating])
-    avg_rating = round(ratings_total / len(ratings), 2)
 
     return render(request,
                   'ratings/movie.html',
-                  {'avg_rating': avg_rating,
-                   'movie': movie,
-                   'num_ratings': len(ratings),
-                   'user_ratings': user_ratings})
+                  {'movie': movie},)
