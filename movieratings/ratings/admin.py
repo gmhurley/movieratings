643e0f2b7cf51b8d9d97b99852bf4d748e19545b
Glenn Hurley <glenn.hurley@gmail.com>
2015-10-09 11:15:01 -0400

    Redo

--- movieratings/ratings/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Rater, Movies, Ratings, Occupation
-
-# Register your models here.
-admin.site.register(Rater)
-admin.site.register(Movies)
-admin.site.register(Ratings)
-admin.site.register(Occupation)
