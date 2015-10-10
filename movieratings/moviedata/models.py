203caf5ffe400fbacd419f89e172dd3086041db6
Glenn Hurley <glenn.hurley@gmail.com>
2015-10-10 11:17:25 -0400

    Created models file.

--- movieratings/moviedata/models.py
+++ movieratings/moviedata/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Movies(models.Model):
+    """Represents the data IRT movies."""
+    title = models.CharField(max_length=255)
+
+
+class Occupation(models.Model):
+    """List of the available occupations."""
+    title = models.CharField(max_length = 55)
+
+
+class Rater(models.Model):
+    """Represents the users that have rated movies."""
+    # Gender::Age::Occupation::Zip-code
+
+    MALE = 'M'
+    FEMALE = 'F'
+    OTHER = 'O'
+    X = 'X'
+
+    GENDER_CHOICES = (
+        (MALE, 'Male',
+         FEMALE, 'Female',
+         OTHER, 'Other',
+         X, 'No answer')
+    )
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    age = models.PositiveSmallIntegerField()
+    occupation = models.ForeignKey(Occupation)
+    zipcode = models.CharField(max_length=10)
+
+
+class Ratings(models.Model):
+    """Represents users ratings of movies."""
+    rating = models.PositiveSmallIntegerField()
+    rater = models.ForeignKey(Rater)
+    movie = models.ForeignKey(Movies)
+
+    def __str__(self):
+        return str(self.id)
